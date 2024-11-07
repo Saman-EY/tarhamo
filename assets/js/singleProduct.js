ed38c51dacc28e8a228a485f40b8af1847ecec9d
Saman <samanezzatabadi@yahoo.com>
2024-11-07 20:38:57 +0330

    utils of slider remaining and mobile nav

--- assets/js/singleProduct.js
+++ assets/js/singleProduct.js
@@ -1,37 +1,90 @@
 let swiper2 = new Swiper(".mySwiper2", {
-    // loop: true,
-    pagination: {
-      el: ".swiper-pagination",
-      clickable: true,
+  // loop: true,
+  pagination: {
+    el: ".swiper-pagination",
+    clickable: true,
+  },
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  breakpoints: {
+    320: {
+      // برای صفحات کوچکتر از 320 پیکسل
+      slidesPerView: 1,
+      spaceBetween: 10,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+    640: {
+      // برای صفحات 640 پیکسل و بالاتر
+      slidesPerView: 2,
+      spaceBetween: 20,
     },
-    breakpoints: {
-      320: {
-        // برای صفحات کوچکتر از 320 پیکسل
-        slidesPerView: 1,
-        spaceBetween: 10,
-      },
-      640: {
-        // برای صفحات 640 پیکسل و بالاتر
-        slidesPerView: 2,
-        spaceBetween: 20,
-      },
-      768: {
-        // برای صفحات 768 پیکسل و بالاتر
-        slidesPerView: 2.5,
-        spaceBetween: 30,
-      },
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30,
-      },
-      1024: {
-        // برای صفحات 1024 پیکسل و بالاتر
-        slidesPerView: 4,
-        spaceBetween: 30,
-      },
+    768: {
+      // برای صفحات 768 پیکسل و بالاتر
+      slidesPerView: 2.5,
+      spaceBetween: 30,
     },
-  });
+    992: {
+      slidesPerView: 3,
+      spaceBetween: 30,
+    },
+    1024: {
+      // برای صفحات 1024 پیکسل و بالاتر
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+  },
+});
+
+//  gallery
+let swiper4 = new Swiper(".mySwiper4", {
+  spaceBetween: 10,
+  watchSlidesProgress: true,
+  breakpoints: {
+    320: {
+      slidesPerView: 2,
+    },
+    420: {
+      slidesPerView: 4,
+    },
+
+    640: {
+      // برای صفحات 640 پیکسل و بالاتر
+      slidesPerView: 3,
+      spaceBetween: 20,
+    },
+    768: {
+      // برای صفحات 768 پیکسل و بالاتر
+      slidesPerView: 4,
+      spaceBetween: 30,
+    },
+    992: {
+      slidesPerView: 5,
+      spaceBetween: 30,
+    },
+  },
+});
+
+// active gallery
+let swiper3 = new Swiper(".mySwiper3", {
+  slidesPerView: 1,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+  thumbs: {
+    swiper: swiper4,
+  },
+});
+
+const customPrevBtn1 = document.getElementById("slidernext1");
+const customNextBtn1 = document.getElementById("sliderprev1");
+
+// Add event listeners to custom buttons
+customPrevBtn1.addEventListener("click", function () {
+  swiper3.slidePrev(); // Go to the previous slide
+});
+
+customNextBtn1.addEventListener("click", function () {
+  swiper3.slideNext(); // Go to the next slide
+});
